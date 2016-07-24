26f8e5031ee663a985c7d463585a69dee675b479
Ben Tsou <ben@bentsou.com>
2016-07-24 10:29:31 -0400

    Update rest-ws.sh

--- rest-ws.sh
+++ rest-ws.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-echo "Hello from Ben"
+
+GIT=`which git`
+GIT_REMOTE="https://github.mlbam.net/btsou/git-demo"
+
+echo "Enter the name of the project:"
+read PROJ_DIR
+
+# Input checks
+if [ -z "$GIT" ]; then
+	echo "git is not installed. Abort."
+	exit 1
+fi
+
+if [ -z "$PROJ_DIR" ]; then
+	echo "Please specify a project name."
+	exit 1
+fi
+
+if [ -d "$PROJ_DIR" ]; then
+	echo "Directory $PROJ_DIR already exists."
+	exit 1
+fi
+
+printf "It's a go! Creating a project @ \e[36m$PROJ_DIR\e[0m from \e[2m$GIT_REMOTE\e[0m\n"
+`$GIT clone $GIT_REMOTE $PROJ_DIR`
+
+# Clean up
+echo "Clean up."
+`rm -rf ./$PROJ_DIR/.git`
+
+echo "All done. 👍"
